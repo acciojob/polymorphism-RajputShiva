ed965b1f0b256fce761284a4d5b2345c770aa60a
Shivaji singh <shivajisingh54@gmail.com>
2023-01-13 12:33:25 +0530

    First Solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,23 +3,23 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
 
-            Product p = new Product();
-             p.Product(15,25);
-             p.Product(10,20,30);
-             p.Product(5.5,6.6);
+        Product p = new Product();
+        p.Product(15,25);
+        p.Product(10,20,30);
+        p.Product(5.5,6.6);
     }
 
 }
 
-  class Product{
-  public int Product(int x,int y){
-   return x*y;
-  }
-  public int Product(int x,int y,int z){
-    return  x*y*z;
-  }
-  public double Product(double x,double y){
-      return  x*y;
-  }
+class Product{
+    public int Product(int x,int y){
+        return x*y;
+    }
+    public int Product(int x,int y,int z){
+        return  x*y*z;
+    }
+    public double Product(double x,double y){
+        return  x*y;
+    }
 
 }

3e7acbee7be4d72abccb1b11648123d7fc11a7c9
Shivaji singh <shivajisingh54@gmail.com>
2023-01-13 21:18:35 +0530

    Solution2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,29 +5,30 @@ public class Main {
 
         Product p = new Product();
 
-        int ansValue = p.product(2,3);
+        int ansValue = p.product(2, 3);
 
-        int secondAnswerValue=p.product(2,3,4);
+        int secondAnswerValue = p.product(2, 3, 4);
 
-        double thirdAnswerValue=p.product(2.1,2.9);
+        double thirdAnswerValue = p.product(2.1, 2.9);
 
     }
 
+   public static class Product {
+        public int product(int x, int y) {
+            int ans = x * y;
+            return ans;
+        }
 
-}
-class Product{
-    public int product(int x, int y) {
-        int ans=x*y;
-        return ans;
-    }
-    public int product(int x, int y, int z) {
+        public int product(int x, int y, int z) {
 
-        int secondAnswer=x*y*z;
-        return  secondAnswer;
-    }
+            int secondAnswer = x * y * z;
+            return secondAnswer;
+        }
 
-    public double product(double x, double y) {
-        double thirdAnswer=x*y;
-        return thirdAnswer;
+        public double product(double x, double y) {
+            double thirdAnswer = x * y;
+            return thirdAnswer;
+        }
+
     }
 }
